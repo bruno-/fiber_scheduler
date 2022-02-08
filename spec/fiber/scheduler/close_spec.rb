d2705686106a5a14487fc9c48c3aad2380145042
Bruno Sutic <code@brunosutic.com>
2022-02-08 18:28:20 +0100

    Add #close spec

---
+++ spec/fiber/scheduler/close_spec.rb
@@ -0,0 +1,41 @@
+require "fiber/scheduler"
+
+RSpec.describe "#close" do
+  context "without #run" do
+    it "" do
+      order = []
+
+      Thread.new do
+        Fiber.set_scheduler(Fiber::Scheduler.new)
+
+        Fiber.schedule do
+          order << 1
+        end
+      end.join
+
+      expect(order).to contain_exactly(1)
+    end
+  end
+
+  context "with #run" do
+    it "" do
+      order = []
+
+      Thread.new do
+        Fiber.set_scheduler(Fiber::Scheduler.new)
+
+        Fiber.schedule do
+          order << 1
+        end
+
+        Fiber.scheduler.run
+
+        Fiber.schedule do
+          order << 2
+        end
+      end.join
+
+      expect(order).to contain_exactly(1, 2)
+    end
+  end
+end

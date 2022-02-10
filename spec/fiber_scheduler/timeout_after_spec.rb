a512bc6d95af20435d1d0b4f30481c24f4e944fb
Bruno Sutic <code@brunosutic.com>
2022-02-10 20:41:42 +0100

    Improve #timeout_after specs

--- spec/fiber_scheduler/timeout_after_spec.rb
+++ spec/fiber_scheduler/timeout_after_spec.rb
@@ -6,8 +6,8 @@ RSpec.shared_examples FiberSchedulerSpec::TimeoutAfter do
   context "Timeout.timeout" do
     let(:order) { [] }
     let(:times) { [] }
-    let(:timeout) { 0.001 }
     let(:duration) { times[1] - times[0] }
+    let(:sleep_duration) { 0.01 }
     let(:operations) do
       -> do
         times << Process.clock_gettime(Process::CLOCK_MONOTONIC)
@@ -16,36 +16,67 @@ RSpec.shared_examples FiberSchedulerSpec::TimeoutAfter do
             order << 1
             Timeout.timeout(timeout) do
               order << 2
-              sleep 1
+              sleep sleep_duration
+              order << 4
             end
+            order << 5
           rescue Timeout::Error
-            order << 4
-            times << Process.clock_gettime(Process::CLOCK_MONOTONIC)
+            order << 6
           end
+          times << Process.clock_gettime(Process::CLOCK_MONOTONIC)
         end
         order << 3
       end
     end
 
-    it "calls #timeout_after" do
-      expect_any_instance_of(scheduler_class)
-        .to receive(:timeout_after)
-        .and_call_original
+    context "when the operation times out" do
+      let(:timeout) { 0.001 }
 
-      setup.call
+      it "calls #timeout_after" do
+        expect_any_instance_of(scheduler_class)
+          .to receive(:timeout_after)
+          .and_call_original
+
+        setup.call
+      end
+
+      it "behaves async" do
+        setup.call
+
+        expect(order).to eq [1, 2, 3, 6]
+      end
+
+      it "times out early" do
+        setup.call
+
+        expect(duration).to be >= timeout
+        expect(duration).to be < (timeout * 10)
+      end
     end
 
-    it "behaves async" do
-      setup.call
+    context "when the operation doesn't time out" do
+      let(:timeout) { 0.1 }
 
-      expect(order).to eq (1..4).to_a
-    end
+      it "calls #timeout_after" do
+        expect_any_instance_of(scheduler_class)
+          .to receive(:timeout_after)
+          .and_call_original
 
-    it "times out early" do
-      setup.call
+        setup.call
+      end
 
-      expect(duration).to be >= timeout
-      expect(duration).to be < (timeout * 10)
+      it "behaves async" do
+        setup.call
+
+        expect(order).to eq (1..5).to_a
+      end
+
+      it "finishes the operation" do
+        setup.call
+
+        expect(duration).to be >= sleep_duration
+        expect(duration).to be < (sleep_duration * 1.5)
+      end
     end
   end
 end

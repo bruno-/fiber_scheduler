997a6a6641a42d5b91e1cb9b55beba3e85c76a53
Bruno Sutic <code@brunosutic.com>
2022-02-11 14:06:57 +0100

    Selector cleanup

--- lib/fiber_scheduler/selector.rb
+++ lib/fiber_scheduler/selector.rb
@@ -1,7 +1,43 @@
 class FiberScheduler
   class Selector
-    def initialize(loop)
-      @loop = loop
+    EAGAIN = Errno::EAGAIN::Errno
+
+    class Waiter
+      def initialize(fiber, events, tail)
+        @fiber = fiber
+        @events = events
+        @tail = tail
+      end
+
+      def alive?
+        @fiber&.alive?
+      end
+
+      def transfer(events)
+        if (fiber = @fiber)
+          @fiber = nil
+
+          fiber.transfer(events & @events) if fiber.alive?
+        end
+
+        @tail&.transfer(events)
+      end
+
+      def invalidate
+        @fiber = nil
+      end
+
+      def each(&block)
+        if (fiber = @fiber)
+          yield fiber, @events
+        end
+
+        @tail&.each(&block)
+      end
+    end
+
+    def initialize(fiber)
+      @fiber = fiber
 
       @waiting = Hash.new.compare_by_identity
 
@@ -11,110 +47,21 @@ class FiberScheduler
       @interrupt = Interrupt.attach(self)
     end
 
-    attr :loop
-
-    # If the event loop is currently blocked,
-    def wakeup
-      if @blocked
-        @interrupt.signal
-
-        return true
-      end
-
-      return false
-    end
-
     def close
       @interrupt.close
 
-      @loop = nil
+      @fiber = nil
       @waiting = nil
-    end
-
-    Optional = Struct.new(:fiber) do
-      def transfer(*arguments)
-        fiber&.transfer(*arguments)
-      end
-
-      def alive?
-        fiber&.alive?
-      end
-
-      def nullify
-        self.fiber = nil
-      end
     end
 
     # Transfer from the current fiber to the event loop.
     def transfer
-      @loop.transfer
-    end
-
-    # Transfer from the current fiber to the specified fiber. Put the current fiber into the ready list.
-    def resume(fiber, *arguments)
-      optional = Optional.new(Fiber.current)
-      @ready.push(optional)
-
-      fiber.transfer(*arguments)
-    ensure
-      optional.nullify
-    end
-
-    # Yield from the current fiber back to the event loop. Put the current fiber into the ready list.
-    def yield
-      optional = Optional.new(Fiber.current)
-      @ready.push(optional)
-
-      @loop.transfer
-    ensure
-      optional.nullify
+      @fiber.transfer
     end
 
     # Append the given fiber into the ready list.
     def push(fiber)
       @ready.push(fiber)
-    end
-
-    # Transfer to the given fiber and raise an exception. Put the current fiber into the ready list.
-    def raise(fiber, *arguments)
-      optional = Optional.new(Fiber.current)
-      @ready.push(optional)
-
-      fiber.raise(*arguments)
-    ensure
-      optional.nullify
-    end
-
-    def ready?
-      !@ready.empty?
-    end
-
-    Waiter = Struct.new(:fiber, :events, :tail) do
-      def alive?
-        self.fiber&.alive?
-      end
-
-      def transfer(events)
-        if fiber = self.fiber
-          self.fiber = nil
-
-          fiber.transfer(events & self.events) if fiber.alive?
-        end
-
-        self.tail&.transfer(events)
-      end
-
-      def invalidate
-        self.fiber = nil
-      end
-
-      def each(&block)
-        if fiber = self.fiber
-          yield fiber, self.events
-        end
-
-        self.tail&.each(&block)
-      end
     end
 
     def io_wait(fiber, io, events)
@@ -125,72 +72,68 @@ class FiberScheduler
       waiter&.invalidate
     end
 
-    if IO.const_defined?(:Buffer)
-      EAGAIN = Errno::EAGAIN::Errno
+    def io_read(fiber, io, buffer, length)
+      offset = 0
 
-      def io_read(fiber, io, buffer, length)
-        offset = 0
+      while true
+        maximum_size = buffer.size - offset
 
-        while true
-          maximum_size = buffer.size - offset
-
-          case result = blocking{io.read_nonblock(maximum_size, exception: false)}
-          when :wait_readable
-            if length > 0
-              self.io_wait(fiber, io, IO::READABLE)
-            else
-              return -EAGAIN
-            end
-          when :wait_writable
-            if length > 0
-              self.io_wait(fiber, io, IO::WRITABLE)
-            else
-              return -EAGAIN
-            end
-          when nil
-            break
+        case result = blocking{io.read_nonblock(maximum_size, exception: false)}
+        when :wait_readable
+          if length > 0
+            self.io_wait(fiber, io, IO::READABLE)
           else
-            buffer.set_string(result, offset)
-
-            size = result.bytesize
-            offset += size
-            break if size >= length
-            length -= size
+            return -EAGAIN
           end
-        end
+        when :wait_writable
+          if length > 0
+            self.io_wait(fiber, io, IO::WRITABLE)
+          else
+            return -EAGAIN
+          end
+        when nil
+          break
+        else
+          buffer.set_string(result, offset)
 
-        return offset
+          size = result.bytesize
+          offset += size
+          break if size >= length
+          length -= size
+        end
       end
 
-      def io_write(fiber, io, buffer, length)
-        offset = 0
+      return offset
+    end
 
-        while true
-          maximum_size = buffer.size - offset
+    def io_write(fiber, io, buffer, length)
+      offset = 0
 
-          chunk = buffer.get_string(offset, maximum_size)
-          case result = blocking{io.write_nonblock(chunk, exception: false)}
-          when :wait_readable
-            if length > 0
-              self.io_wait(fiber, io, IO::READABLE)
-            else
-              return -EAGAIN
-            end
-          when :wait_writable
-            if length > 0
-              self.io_wait(fiber, io, IO::WRITABLE)
-            else
-              return -EAGAIN
-            end
+      while true
+        maximum_size = buffer.size - offset
+
+        chunk = buffer.get_string(offset, maximum_size)
+        case result = blocking{io.write_nonblock(chunk, exception: false)}
+        when :wait_readable
+          if length > 0
+            self.io_wait(fiber, io, IO::READABLE)
           else
-            offset += result
-            break if result >= length
-            length -= result
+            return -EAGAIN
           end
+        when :wait_writable
+          if length > 0
+            self.io_wait(fiber, io, IO::WRITABLE)
+          else
+            return -EAGAIN
+          end
+        else
+          offset += result
+          break if result >= length
+          length -= result
         end
-
-        return offset
       end
+
+      return offset
     end
 
     def process_wait(fiber, pid, flags)
@@ -209,19 +152,6 @@ class FiberScheduler
       r.close
       w.close
       thread&.kill
-    end
-
-    private def pop_ready
-      unless @ready.empty?
-        count = @ready.size
-
-        count.times do
-          fiber = @ready.pop
-          fiber.transfer if fiber.alive?
-        end
-
-        return true
-      end
     end
 
     def select(duration = nil)
@@ -267,7 +197,22 @@ class FiberScheduler
       return ready.size
     end
 
-    private def blocking(&block)
+    private
+
+    def pop_ready
+      unless @ready.empty?
+        count = @ready.size
+
+        count.times do
+          fiber = @ready.pop
+          fiber.transfer if fiber.alive?
+        end
+
+        return true
+      end
+    end
+
+    def blocking(&block)
       Fiber.new(blocking: true, &block).resume
     end
   end
@@ -292,16 +237,14 @@ class FiberScheduler
       @fiber.transfer
     end
 
-    # Send a sigle byte interrupt.
     def signal
-      @output.write('.')
+      @output.write(".")
       @output.flush
     end
 
     def close
       @input.close
       @output.close
-      # @fiber.raise(::Interrupt)
     end
   end
 end

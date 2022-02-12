a9b87083c8acad6599c22418924a7100620f8a3d
Bruno Sutic <code@brunosutic.com>
2022-02-12 14:36:19 +0100

    Improve async compatibility

--- spec/fiber_scheduler/async_compatibility_spec.rb
+++ spec/fiber_scheduler/async_compatibility_spec.rb
@@ -1,49 +1,168 @@
 require "async"
 
 RSpec.describe FiberScheduler do
-  shared_examples :async_compatibility do
+  describe "async compatibility" do
     include_context FiberSchedulerSpec::Context
 
     let(:order) { [] }
 
-    context "without FiberScheduler options" do
-      it "behaves synchronous" do
-        Async do |task|
-          order << 1
-          FiberScheduler do # default 'wait: true' option
-            order << 2
-            Fiber.schedule do
-              order << 3
-              sleep 0.001
-              order << 7
-            end
-
-            order << 4
-
-            Fiber.schedule do
-              order << 5
+    context "with Async block" do
+      context "without FiberScheduler options" do
+        it "behaves synchronous" do
+          Async do |task|
+            task.async do
+              order << 1
               sleep 0.001
               order << 8
             end
 
-            order << 6
-            sleep 0.02
-            order << 9
+            order << 2
+
+            FiberScheduler do # default 'wait: true' option
+              order << 3
+              Fiber.schedule do
+                order << 4
+                sleep 0.001
+                order << 9
+              end
+
+              order << 5
+
+              Fiber.schedule do
+                order << 6
+                sleep 0.001
+                order << 10
+              end
+
+              order << 7
+              sleep 0.02
+              order << 11
+            end
+
+            order << 12
           end
 
-          order << 10
+          expect(order).to eq (1..12).to_a
         end
+      end
 
-        expect(order).to eq (1..10).to_a
+      context "with a blocking FiberScheduler" do
+        it "blocks all other async tasks" do
+          Async do |task|
+            order << 1
+
+            task.async do
+              order << 2
+              sleep 0.01
+              order << 8
+            end
+
+            order << 3
+
+            FiberScheduler(blocking: true) do
+              order << 4
+              Fiber.schedule do
+                order << 5
+                sleep 0.01
+                order << 9
+              end
+              sleep 0.02
+              order << 6
+            end
+
+            order << 7
+          end
+
+          expect(order).to eq (1..9).to_a
+        end
+      end
+
+      context "with a non-waiting FiberScheduler" do
+        it "behaves async" do
+          Async do |task|
+            order << 1
+
+            task.async do
+              order << 2
+              sleep 0.01
+              order << 8
+            end
+
+            order << 3
+
+            FiberScheduler(wait: false) do
+              order << 4
+              Fiber.schedule do
+                order << 5
+                sleep 0.01
+                order << 9
+              end
+              order << 7
+              sleep 0.02
+              order << 10
+            end
+
+            order << 6
+          end
+
+          expect(order).to eq (1..10).to_a
+        end
       end
     end
 
-    context "with a blocking FiberScheduler" do
-      it "blocks all other async tasks" do
-        Async do |task|
+    context "with Async::Scheduler" do
+      context "without FiberScheduler options" do
+        it "behaves synchronous" do
+          scheduler = Async::Scheduler.new
+          Fiber.set_scheduler scheduler
+
+          Fiber.schedule do
+            order << 1
+            sleep 0.001
+            order << 8
+          end
+
+          order << 2
+
+          FiberScheduler do # default 'wait: true' option
+            order << 3
+            Fiber.schedule do
+              order << 4
+              sleep 0.001
+              order << 9
+            end
+
+            order << 5
+
+            Fiber.schedule do
+              order << 6
+              sleep 0.001
+              order << 10
+            end
+
+            order << 7
+            sleep 0.02
+            order << 11
+          end
+
+          order << 12
+
+          scheduler.run
+
+          order << 13
+
+          expect(order).to eq (1..13).to_a
+        end
+      end
+
+      context "with a blocking FiberScheduler" do
+        it "blocks all other async tasks" do
+          scheduler = Async::Scheduler.new
+          Fiber.set_scheduler scheduler
+
           order << 1
 
-          task.async do
+          Fiber.schedule do
             order << 2
             sleep 0.01
             order << 8
@@ -59,22 +178,25 @@ RSpec.describe FiberScheduler do
               order << 9
             end
             sleep 0.02
-            order << 6
+            order << 7
           end
 
-          order << 7
+          order << 6
+
+          scheduler.run
+
+          expect(order).to eq (1..9).to_a
         end
-
-        expect(order).to eq (1..9).to_a
       end
-    end
 
-    context "with a non-waiting FiberScheduler" do
-      it "behaves async" do
-        Async do |task|
+      context "with a non-waiting FiberScheduler" do
+        it "behaves async" do
+          scheduler = Async::Scheduler.new
+          Fiber.set_scheduler scheduler
+
           order << 1
 
-          task.async do
+          Fiber.schedule do
             order << 2
             sleep 0.01
             order << 8
@@ -95,13 +217,12 @@ RSpec.describe FiberScheduler do
           end
 
           order << 6
-        end
 
-        expect(order).to eq (1..10).to_a
+          scheduler.run
+
+          expect(order).to eq (1..10).to_a
+        end
       end
     end
-  end
-
-  describe "async compatibility" do
   end
 end
